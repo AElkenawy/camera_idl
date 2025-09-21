ba9d93371543e42e219caa1a1fa5f4e72e49b765
Amr Elkenawy <amora.samy@gmail.com>
2025-09-21 17:05:56 +0300

    adjust protobuf path

--- build.sh
+++ build.sh
@@ -5,31 +5,32 @@ TARGET=$1
 PROTO_DIR="./protos"
 BUILD_DIR="./gen"
 
+PROTOC=../third_party/grpc/third_party/protobuf/build/protoc
+GRPC_CPP_PLUGIN=`which grpc_cpp_plugin`
+
 if [ -z "$TARGET" ]; then
   echo "Usage: $0 {cpp|dart|python}"
   exit 1
 fi
 
+mkdir -p "$BUILD_DIR"
 
 case "$TARGET" in
   cpp)
-    mkdir -p $BUILD_DIR
     for f in $PROTO_DIR/*.proto; do
-        protoc -I $PROTO_DIR --cpp_out=$BUILD_DIR/ $f
-        protoc -I $PROTO_DIR --grpc_out=$BUILD_DIR/ --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $f
+        $PROTOC -I $PROTO_DIR --cpp_out=$BUILD_DIR $f
+        $PROTOC -I $PROTO_DIR --grpc_out=$BUILD_DIR --plugin=protoc-gen-grpc=$GRPC_CPP_PLUGIN $f
     done
     echo "C++ code generated in $BUILD_DIR/"
     ;;
 
   dart)
-    mkdir -p ../lib/$BUILD_DIR
     export PATH="$PATH":"path/to/workspace-automation/.config/flutter_workspace/pub_cache/bin"
-    protoc --dart_out=grpc:../lib/$BUILD_DIR/ -I$PROTO_DIR $PROTO_DIR/*.proto
-    echo "Dart code generated in ../lib/$BUILD_DIR/"
+    protoc --dart_out=grpc:$BUILD_DIR/ -I$PROTO_DIR $PROTO_DIR/*.proto
+    echo "Dart code generated in $BUILD_DIR/"
     ;;
 
   python)
-    mkdir -p $BUILD_DIR
     python3 -m grpc_tools.protoc -I$PROTO_DIR \
       --python_out=$BUILD_DIR/ \
       --grpc_python_out=$BUILD_DIR/ \
